60bf3ea16180dc1384b3f9fff0d56c67e844a57a
shi-lai-mu <478889187@qq.com>
2021-04-03 20:33:12 +0800

    feat: 追加svg图标

---
+++ src/menus/icon.ts
@@ -0,0 +1,9 @@
+import fontColor from '../assets/svg/fontColor.svg';
+import fontHead from '../assets/svg/fontHead.svg';
+import fontBold from '../assets/svg/fontBold.svg';
+
+export {
+  fontColor,
+  fontHead,
+  fontBold,
+}
